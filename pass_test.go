226cb52267443d2b379151478048a02e98d68d8a
Lachlan Donald <lachlan@ljd.cc>
2019-05-19 14:57:56 +1000

    Remove mentions of aws-vault

--- pass_test.go
+++ pass_test.go
@@ -41,7 +41,7 @@ func setup(t *testing.T) (*passKeyring, func(t *testing.T)) {
 	k := &passKeyring{
 		dir:     passdir,
 		passcmd: "pass",
-		prefix:  "aws-vault",
+		prefix:  "keyring",
 	}
 
 	cmd, err = k.pass("init", "test@example.com")
